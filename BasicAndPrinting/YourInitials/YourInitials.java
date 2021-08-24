7041571fb47f7bc2e1c715c7259546c20b8b53ec
nayeon131 <89062342+nayeon131@users.noreply.github.com>
2021-08-24 14:39:51 +0800

    YourInitials
    YourInitials

---
+++ BasicAndPrinting/YourInitials/YourInitials.java
@@ -0,0 +1,44 @@
+public class YourInitials
+{
+	public static void main ( String[] args )
+	{
+
+		System.out.println ( "k             k" ) ;
+		System.out.println ( "k           k" ) ;
+		System.out.println ( "k         k" ) ;
+		System.out.println ( "k      k" ) ;
+		System.out.println ( "k   k" ) ;
+		System.out.println ( "k     k" ) ;
+		System.out.println ( "k       k" ) ;
+		System.out.println ( "k         k" ) ;
+		System.out.println ( "k           k" ) ;
+
+		System.out.println ( "n         	 n" ) ;
+		System.out.println ( "nn         	 n" ) ;
+		System.out.println ( "n  n        n" ) ;
+		System.out.println ( "n    n      n" ) ;
+		System.out.println ( "n      n    n" ) ;
+		System.out.println ( "n        n	 n" ) ;
+		System.out.println ( "n          nn" ) ;
+		System.out.println ( "n         	 n" ) ;
+
+		System.out.println ( "y         	 y" ) ;
+		System.out.println ( "  y        y" ) ;
+		System.out.println ( "    y   y" ) ;
+		System.out.println ( "      y" ) ;
+		System.out.println ( "      y" ) ;
+		System.out.println ( "      y" ) ;
+		System.out.println ( "      y" ) ;
+
+	 }
+}
+
+
+
+
+
+
+
+
+
+		
