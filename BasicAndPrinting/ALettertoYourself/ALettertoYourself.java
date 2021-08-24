dc5305648a1aca990d936a608d37c2bab2b0b8f9
nayeon131 <89062342+nayeon131@users.noreply.github.com>
2021-08-24 14:17:43 +0800

    ALettertoYourself
    ALettertoYourself

---
+++ BasicAndPrinting/ALettertoYourself/ALettertoYourself.java
@@ -0,0 +1,18 @@
+public class ALettertoYourself
+{
+	public static void main ( String[] args )
+	{
+	
+		System.out.println ( "+---------------------------------------------------------+" ) ;
+		System.out.println ( "|                                                                                                    #### |" ) ;
+	 	System.out.println ( "|                                                                                                    #### |" ) ;	
+		System.out.println ( "|                                                                                                    #### |" ) ;
+		System.out.println ( "|                                                     	               		                    |" ) ;
+		System.out.println ( "|                             		                Bill Gates            	     |" ) ;
+		System.out.println ( "|                              		 	 1 Microsoft Way          |" ) ;
+		System.out.println ( "|                             			 Redmond, WA 98104    |" ) ;
+		System.out.println ( "|                                                     				     |" ) ;
+		System.out.println ( "+---------------------------------------------------------+" ) ;
+
+	 }
+}
