e1efd7b07cf02936b4e60d29003d4b154ab4e1f7
nayeon131 <89062342+nayeon131@users.noreply.github.com>
2021-08-25 22:29:42 +0800

    StillUsingVariables
    //Variables Still Using Variables 10 points Nayeon

---
+++ Variables/StillUsingVariables/StillUsingVariables.java
@@ -0,0 +1,16 @@
+public class StillUsingVariables
+{
+	public static void main ( String[] args )
+	{
+	     	String myName;
+		int graduate;
+
+		myName = "Juan Valdez";
+		graduate = 2010;
+
+		System.out.println( "My name is " + myName + "." );
+		System.out.println( "I'll graduate" + graduate + "." );
+	}
+
+}
+	
