6418ff11f0343d2c3169aad984d20ffaaa2d785c
nayeon131 <89062342+nayeon131@users.noreply.github.com>
2021-10-15 14:14:30 +0800

    Nested Loops_Basic Nested Loops_50 points_Nayeon
    Nested Loops_Basic Nested Loops_50 points_Nayeon

---
+++ NestedLoops/BasicNestedLoops/BasicNestedLoops.java
@@ -0,0 +1,13 @@
+public class BasicNestedLoops
+{
+  public static void main (String[] args )
+  { System.out.println();
+    for ( int i=0 ; i<=5; i++)
+    {
+      for ( int j=0 ; j<=5; j++)
+      {
+          System.out.print ("("+i+","+j+")") ;
+      }
+    }
+  }
+}
