4cc0847a288fc158139cd65d2bbffa0f08ee94d1
nayeon131 <89062342+nayeon131@users.noreply.github.com>
2021-11-12 14:31:07 +0800

    Arrays_Basic1_30 points_Nayeon
    Arrays_Basic1_30 points_Nayeon

---
+++ Arrays /Basic1/Basic1.java
@@ -0,0 +1,12 @@
+public class Basic1
+{
+  public static void main (String[] args)
+  {
+    int a[] = new int [10];
+    for (int i=0; i<a.length; i++ )
+    {
+      System.out.println ("Slot "+i+" conatains a -113");
+    }
+
+  }
+ }
