64c37831103914398c90296c3f41a72020043727
nayeon131 <89062342+nayeon131@users.noreply.github.com>
2021-11-30 14:08:45 +0800

    Records_Web adresses array_10 points_ Nayeon
    Records_Web adresses array_10 points_ Nayeon

---
+++ Records/Webadressesarray/Webadressesarray.java
@@ -0,0 +1,38 @@
+import java.net.URL;
+import java.util.Scanner;
+
+class Address
+{
+	String street;
+	String city;
+	String state;
+	int zip;
+}
+
+public class Webadressesarray
+{
+	public static void main(String[] args) throws Exception
+	{
+		URL addys = new URL("https://programmingbydoing.com/a/examples/fake-addresses.txt");
+
+		Address[] addybook = new Address[6];
+
+		Scanner fin = new Scanner( addys.openStream() );
+
+		for ( int i=0; i<addybook.length; i++ )
+		{
+			addybook[i] = new Address();
+			addybook[i].street = fin.nextLine();
+			addybook[i].city   = fin.nextLine();
+			addybook[i].state  = fin.next();
+			addybook[i].zip    = fin.nextInt();
+			fin.skip("\n");
+		}
+		fin.close();
+
+		for ( int i=0; i<5; i++ )
+		{
+			System.out.println((i+1) + ". " + addybook[i].street + ", " + addybook[i].city + ", " + addybook[i].state + "  " + addybook[i].zip);
+		}
+	}
+}
