c7406052ef51665333723381e091a8bbe313df4a
nayeon131 <89062342+nayeon131@users.noreply.github.com>
2021-12-10 14:08:49 +0800

    Arayylist_Finding a value in an arraylist_80 points_Nayeon
    Arayylists_Finding a value in an arraylist_80 points_Nayeon

---
+++ Arraylist/Finding/Finding.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+ public class Finding
+ {
+   public static void main (String[] args)
+   {
+
+     ArrayList <Integer> mylist = new ArrayList<Integer>();
+     Random generator = new Random();
+     Scanner input = new Scanner (System.in);
+
+     for (int i = 0 ;  i < 10; i++)
+     {
+       mylist.add (1+generator.nextInt(51));
+     }
+
+     System.out.println (mylist);
+     int x = input.nextInt();
+     System.out.println ("Value to find " + x );
+
+
+     for (Integer i : mylist)
+     {
+       if (x==i)
+       {
+         System.out.println (x + " is in");
+       }
+     }
+
+   }
+ }
